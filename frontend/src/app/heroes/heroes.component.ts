494cd4287750abbf412ce4f04126a654059aa99a
robdev1980 <robert.nimax@gmail.com>
2023-04-13 14:14:25 +0200

    Chapter 2

--- frontend/src/app/heroes/heroes.component.ts
+++ frontend/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,5 +8,11 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  public hero: Hero = { id: 1, name: 'Windstorm' };
+  public heroes: Hero[] = HEROES;
+
+  public onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  public selectedHero?: Hero;
 }
